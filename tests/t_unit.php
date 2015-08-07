1f22f15f7137b14eb6fbb043ccad88532b8a06df
rmouremb <rmouremb@LFR407906.corp.capgemini.com>
2015-08-07 16:50:33 +0200

    Add test on RestoFeature Correct covers action

--- tests/t_unit.php
+++ tests/t_unit.php
@@ -15,10 +15,6 @@
 class t_unit extends RestoUnitTest {
     // ...
 
-    /**
-     * @covers RestoLicense::setDescription
-     * @covers RestoLicense::__construct
-     */
     public function testCreateLicense() {
         $this->initContext();
         $file_license = file_get_contents(dirname(__FILE__) . "/../data/license_Example.json");
@@ -30,8 +26,6 @@ class t_unit extends RestoUnitTest {
     /**
      * @depends testCreateLicense
      * 
-     * @covers RestoCollection::__construct
-     * @covers RestoCollection::loadFromJSON
      */
     public function testCreateCollection() {
         $this->initContext();
@@ -49,8 +43,6 @@ class t_unit extends RestoUnitTest {
     /**
      * @depends testCreateCollection
      * 
-     * @covers RestoCollection::__construct
-     * @covers RestoCollection::addFeature
      */
     public function testInsertResource() {
         $this->initContext();
@@ -59,33 +51,54 @@ class t_unit extends RestoUnitTest {
         $collection = new RestoCollection($data['name'], $this->context, $this->admin, array('autoload' => true));
         $data = file_get_contents(dirname(__FILE__) . "/../data/LANDSAT5_TM_XS_20110520_N2A_France-MetropoleD0005H0003.xml");
         $collection->addFeature(array($data));
+        
+        
+    }
+    
+    /**
+     * @depends testInsertResource
+     */
+    public function testResource(){
+        $this->initContext();
+        $data = file_get_contents(dirname(__FILE__) . "/../data/Landsat.json");
+        $data = json_decode($data, true);
+        $collection = new RestoCollection($data['name'], $this->context, $this->admin, array('autoload' => true));
+        $feature = new RestoFeature($this->context, $this->admin, array(
+            'featureIdentifier' => 'c5dc1f32-002d-5ee9-bd4a-c690461eb734',
+            'collection' => $collection
+        ));
+        $this->assertEquals(true, $feature->isValid());
+        $license = $feature->getLicense();
+        $this->assertEquals('Example', $license->licenseId);
     }
 
     /**
      * @depends testInsertResource
      * 
-     * @covers RestoCollection::__construct
-     * @covers RestoCollection::getUrl
-     * @covers RestoCollection::getStatistics
-     * @covers RestoCollection::toJSON
      */
     public function testGetCollection() {
         $this->initContext();
-        $profile = array(
-            'userid' => 2,
-            'email' => 'toto',
-            'groups' => 'toto',
-            'activated' => 1
-        );
-
-        $user = new RestoUser($profile, $this->context);
-
-        $collection = new RestoCollection('Example', $this->context, $user, array('autoload' => true));
+        $collection = new RestoCollection('Example', $this->context, $this->admin, array('autoload' => true));
         $this->assertEquals('Example', $collection->name);
         $this->assertEquals('http:///resto/collections/Example', $collection->getUrl());
-
-
-        $collection = new RestoCollection('Landsat', $this->context, $user, array('autoload' => true));
+    }
+    
+    /**
+     * @depends testGetCollection
+     */
+    public function testCollection_toFeatureId(){
+        $this->initContext();
+        $collection = new RestoCollection('Landsat', $this->context, $this->admin, array('autoload' => true));
+        $this->assertEquals('c5dc1f32-002d-5ee9-bd4a-c690461eb734', $collection->toFeatureId('LANDSAT5_TM_XS_20110520_N2A_France-MetropoleD0005H0003'));
+    }
+    
+    /**
+     * @depends testGetCollection
+     * 
+     */
+    public function testStatsCollection(){
+        $this->initContext();
+        $collection = new RestoCollection('Landsat', $this->context, $this->admin, array('autoload' => true));
         $this->assertEquals('{"collection":{"Landsat":1},"instrument":{"TM":1},"platform":{"LANDSAT5":1},"processingLevel":{"LEVEL2A":1},"productType":{"REFLECTANCE":1},"sensorMode":{"XS":1}}', json_encode($collection->getStatistics()));
         //$this->assertEquals('{"name":"Landsat","status":"public","owner":"admin","model":"RestoModel_muscate","license":{"licenseId":"Example","hasToBeSigned":"once","grantedCountries":"FR,UK,SP","grantedOrganizationCountries":"FR,UK,SP,DE","grantedFlags":"REGISTERED","viewService":"public","signatureQuota":-1,"description":{"shortName":"Sentinel 1 Data","url":"https:\/\/sentinel.esa.int\/documents\/247904\/690755\/TC_Sentinel_Data_31072014.pdf"}},"osDescription":{"fr":{"ShortName":"Landsat","LongName":"Images Landsat Niveau 2A","Description":"Images de r\u00e9flectance Landsat (Level 2A) trait\u00e9es par le <a href=\"http:\/\/www.theia-land.fr\">p\u00f4le Theia<\/a> pour l\'Agence Spatiale Fran\u00e7aise (<a href=\"http:\/\/www.cnes.fr\">CNES<\/a>). L\'atelier de production d\u00e9velopp\u00e9 par le CNES utilise le prototype de cha\u00eene de Niveau 2A, MACCS, d\u00e9velopp\u00e9 et con\u00e7u au CESBIO. Les donn\u00e9es LANDSAT 8 L1T sont librement accessibles et fournies par <a href=\"http:\/\/earthexplorer.usgs.gov\">l\'USGS<\/a>","Tags":"landsat niveau2A reflectance muscate CNES","Developper":"J\u00e9r\u00f4me Gasperi","Contact":"jerome.gasperi@cnes.fr","Query":"Images sur la France entre octobre et decembre 2013","Attribution":"CNES. Copyright 2014, All Rights Reserved"},"en":{"ShortName":"Landsat","LongName":"Landsat Level2A images","Description":"Reflectance Landsat images (Level 2A) processed by the <a href=\"http:\/\/www.theia-land.fr\">Theia Land Data Center<\/a> for the French Space Agency (<a href=\"http:\/\/www.cnes.fr\">CNES<\/a>). The processing center developped by CNES uses the MACCS prototype L2A chain developped and designed by CESBIO. LANDSAT 8 L1T Input data come from <a href=\"http:\/\/earthexplorer.usgs.gov\">USGS<\/a>, that we would like to thank for releasing freely the LANDSAT 8 datasets","Tags":"landsat level2A reflectance muscate CNES","Developper":"J\u00e9r\u00f4me Gasperi","Contact":"jerome.gasperi@cnes.fr","Query":"Images acquired in France between october and december 2013","Attribution":"CNES. Copyright 2014, All Rights Reserved"}},"statistics":{"collection":{"Landsat":1},"instrument":{"TM":1},"platform":{"LANDSAT5":1},"processingLevel":{"LEVEL2A":1},"productType":{"REFLECTANCE":1},"sensorMode":{"XS":1}}}', $collection->toJSON());
     }
@@ -243,9 +256,6 @@ class t_unit extends RestoUnitTest {
 
     /**
      * @depends testGetCollection
-     * 
-     * @covers RestoUser::isAdmin
-     * @covers RestoUser::hasRightsTo
      */
     public function testAdminUser() {
 
@@ -261,13 +271,6 @@ class t_unit extends RestoUnitTest {
 
     /**
      * @depends testGetCollection
-     * 
-     * @covers RestoUser::__construct
-     * @covers RestoUser::isAdmin
-     * @covers RestoUser::hasRightsTo
-     * @covers RestoLicense::__construct
-     * @covers RestoLicense::isApplicableToUser
-     * @covers RestoLicense::hasToBeSignedByUser
      */
     public function testUnregisteredUser() {
 
@@ -298,15 +301,6 @@ class t_unit extends RestoUnitTest {
 
     /**
      * @depends testGetCollection
-     * 
-     * @covers RestoUser::__construct
-     * @covers RestoUser::isAdmin
-     * @covers RestoUser::hasRightsTo
-     * @covers RestoUser::isValidated
-     * @covers RestoUser::signLicense
-     * @covers RestoLicense::__construct
-     * @covers RestoLicense::isApplicableToUser
-     * @covers RestoLicense::hasToBeSignedByUser
      */
     public function testRegisteredUser() {
 
@@ -362,9 +356,6 @@ class t_unit extends RestoUnitTest {
     /**
      * @depends testInsertResource
      * 
-     * @covers RestoCollection::__construct
-     * @covers RestoFeature::__construct
-     * @covers RestoFeature::removeFromStore
      */
     public function testRemoveResource() {
         $this->initContext();
@@ -383,8 +374,6 @@ class t_unit extends RestoUnitTest {
     /**
      * @depends testRemoveResource
      * 
-     * @covers RestoCollection::__construct
-     * @covers RestoCollection::removeFromStore
      */
     public function testRemoveCollection() {
         $this->initContext();
@@ -395,10 +384,7 @@ class t_unit extends RestoUnitTest {
         $collection->removeFromStore();
     }
 
-    /**
-     * @covers RestoDatabaseDriver_PostgreSQL::remove
-     * @covers Functions_licenses::removeLicense
-     */
+
     public function testRemoveLicense() {
         $this->initContext();
         $file_license = file_get_contents(dirname(__FILE__) . "/../data/license_Example.json");
